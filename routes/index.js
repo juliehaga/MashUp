f5e8beffdc2136fba7c005edf74af9231cb351b0
juliehaga <jul.haga@gmail.com>
2018-09-04 06:47:29 +0200

    static routing added

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Find Activities' });
+});
+
+
+router.get('/about', function(req, res){
+    res.render('about');
+});
+
+
+router.get('/find-activities', function(req, res){
+    res.render('find-activities');
+});
+
+module.exports = router;
